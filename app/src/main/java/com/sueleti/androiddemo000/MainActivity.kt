caec04c9e6cd3aa039bff8b9bdf41d2a11b0a796
jose <jose.hernandez@hcltech.com>
2025-08-07 18:43:15 +0200

    Lección 2 - Layouts Agregar varios textos, importar imagen y modificarla, separadores

--- app/src/main/java/com/sueleti/androiddemo000/MainActivity.kt
+++ app/src/main/java/com/sueleti/androiddemo000/MainActivity.kt
@@ -3,8 +3,22 @@ package com.sueleti.androiddemo000
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 /** setContent is a function that sets the content view of the activity to a composable function.
  * In this case, it sets the content to a Text composable that displays "Hello world!".
@@ -19,17 +33,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessaggeCard("Android")
+            MessaggeCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
+
+data class Message(val author: String, val body: String)
 
 /** A fin de que una función admita composición, debes agregar la anotación @Composable.
  * Para probarlo, define una función MessageCard a la que se le da un nombre que usa para configurar el elemento de texto.
  */
 @Composable
-fun MessaggeCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessaggeCard(msg: Message) {
+    Row (modifier = Modifier.padding(all = 10.dp)){
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier.size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 
